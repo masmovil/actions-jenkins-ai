20afb1172615543e41fbcd11317214f28a605c33
Nicanor Romero Venier <nicanor.romero@masmovil.com>
2025-07-05 23:34:11 +0200

    Install requirements in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ export SLACK_ACCESS_TOKEN
 export SLACK_MESSAGE_THREAD_TS
 export SLACK_CHANNEL
 
+pip3 install --no-cache-dir -r /requirements.txt
 python3 /main.py
 
 echo 'Running entrypoint done'
